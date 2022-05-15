b9334eec96f4a17d8277024b79e176d78c5d700f
João Damaceno <joaodamaceno188@gmail.com>
2022-05-15 19:43:10 -0300

    First App in React and TypScript

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/Listitem/index';
+import { AddArea } from './components/AddArea';
+
+
+const App = () => {
+  const [list, setlist] = useState<Item[]>([
+    {id: 1, name: "Fazer coisas ae", done: false},
+    {id: 2, name: "Dahora", done: false}
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setlist(newList);
+  }
+  return (
+    <C.Container>
+      <C.Area>
+    
+        <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea onEnter={handleAddTask} />
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item} />
+        ))}
+      </C.Area>
+    </C.Container>
+  );
+};
+
+export default App;
